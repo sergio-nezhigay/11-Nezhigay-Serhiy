0eaa22c7d164c3cfb135b426ea353fda18d98cc9
Sergio Nezhigay <abc1971h@gmail.com>
2023-04-02 10:29:22 +0300

    add output

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
+const resultEl = document.querySelector(".result");
+let task = "<h1> HomeWork #11 </h1>\n";
+
 const array = [1, 2, 3, 4, 5, 6, 7];
 removeElement(array, 5);
 console.log(array);
 // Результат: [1, 2, 3, 4, 6, 7]
 
 function removeElement(inputArray, index) {
-  return inputArray.splice(index - 1, 1);
+  inputArray.splice(index - 1, 1);
 }
+
+task += array;
+
+resultEl.insertAdjacentHTML("beforeend", task);
